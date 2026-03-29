eb2792fb8c0853db10b7b7716417a3db3db867e3
sara-prioriello <saraprioriello6@gmail.com>
2026-03-29 17:28:25 +0200

    add snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((element) =>{
+    console.log(element);
+    
+})
