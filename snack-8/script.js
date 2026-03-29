5b6cd68fd2991df80321d42cc31a3debd1a5144d
sara-prioriello <saraprioriello6@gmail.com>
2026-03-29 18:01:45 +0200

    add snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const foundStudent = students.find((element) => element.name === 'Marco Lanci')
+const classStudent = foundStudent.class;
+
+console.log(classStudent);
