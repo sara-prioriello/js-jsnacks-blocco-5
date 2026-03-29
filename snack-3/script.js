9ca8c48dcd2de6f2923c88a65ce341ea4c12c094
sara-prioriello <saraprioriello6@gmail.com>
2026-03-29 17:45:26 +0200

    add snacks3

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,3 +3,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
+
+
+ let newNumbers = numbers.map(function(element){
+    return element + 1;
+   
+     
+})
+
+console.log(newNumbers);
